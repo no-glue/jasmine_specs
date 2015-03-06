4fe9a627b17b6f02557f8c2ef1411af51b865a21
no-glue <nikolapav1985@gmail.com>
2015-03-06 23:54:27 +0100

    collection spec

--- specs/collection-spec.js
+++ specs/collection-spec.js
@@ -1,8 +1,35 @@
 var Collection = require("../collection");
 describe("length", function() {
-  it("the length should be 1 after push", function() {
+  it("should be initially empty", function() {
+    var collection = new Collection();
+    expect(collection.length()).toBe(0);
+  });
+  it("should be 1 after push", function() {
     var collection = new Collection();
     collection.push(0);
     expect(collection.length()).toBe(1);
+  });
+  it("should be empty after pop", function() {
+    var collection = new Collection();
+    collection.push(0);
+    collection.pop();
+    expect(collection.length()).toBe(0);
+  });
+});
+describe("first", function() {
+  it("should be initially undefined", function() {
+    var collection = new Collection();
+    expect(collection.first()).toBe(undefined);
+  });
+  it("should return first element", function() {
+    var collection = new Collection();
+    collection.push(0);
+    expect(collection.first()).toBe(0);
+  });
+  it("should return first element if there are multiple elements", function() {
+    var collection = new Collection();
+    collection.push(0);
+    collection.push(1);
+    expect(collection.first()).toBe(0);
   });
 });
